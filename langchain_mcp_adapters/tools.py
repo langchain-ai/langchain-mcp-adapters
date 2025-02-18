7e63dff6ee204c50da9a0d17eef612729120f2ad
vbarda <vadym@langchain.dev>
2025-02-18 10:32:39 -0500

    improve typing

--- langchain_mcp_adapters/tools.py
+++ langchain_mcp_adapters/tools.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from mcp.types import (
     Tool as MCPTool,
     CallToolResult,
@@ -6,7 +8,7 @@ from mcp.types import (
     EmbeddedResource,
 )
 from mcp import ClientSession
-from langchain_core.tools import StructuredTool, ToolException
+from langchain_core.tools import BaseTool, StructuredTool, ToolException
 
 
 NonTextContent = ImageContent | EmbeddedResource
@@ -23,7 +25,7 @@ def _convert_call_tool_result(
         else:
             non_text_contents.append(content)
 
-    tool_content = [content.text for content in text_contents]
+    tool_content: str | list[str] = [content.text for content in text_contents]
     if len(text_contents) == 1:
         tool_content = tool_content[0]
 
@@ -33,11 +35,11 @@ def _convert_call_tool_result(
     return tool_content, non_text_contents or None
 
 
-def convert_mcp_tool_to_structured_tool(
+def convert_mcp_tool_to_langchain_tool(
     session: ClientSession,
     tool: MCPTool,
-) -> StructuredTool:
-    """Convert an MCP tool to a LangChain StructuredTool.
+) -> BaseTool:
+    """Convert an MCP tool to a LangChain tool.
 
     NOTE: this tool can be executed only in a context of an active MCP client session.
 
@@ -46,23 +48,25 @@ def convert_mcp_tool_to_structured_tool(
         tool: MCP tool to convert
 
     Returns:
-        a LangChain StructuredTool
+        a LangChain tool
     """
 
-    async def call_tool(**arguments):
+    async def call_tool(
+        **arguments: dict[str, Any],
+    ) -> tuple[str | list[str], list[NonTextContent] | None]:
         call_tool_result = await session.call_tool(tool.name, arguments)
         return _convert_call_tool_result(call_tool_result)
 
     return StructuredTool(
         name=tool.name,
-        description=tool.description,
+        description=tool.description or "",
         args_schema=tool.inputSchema,
         coroutine=call_tool,
         response_format="content_and_artifact",
     )
 
 
-async def load_mcp_tools(session: ClientSession) -> list[StructuredTool]:
-    """Load all available MCP tools and convert them to StructuredTools."""
+async def load_mcp_tools(session: ClientSession) -> list[BaseTool]:
+    """Load all available MCP tools and convert them to LangChain tools."""
     tools = await session.list_tools()
-    return [convert_mcp_tool_to_structured_tool(session, tool) for tool in tools.tools]
+    return [convert_mcp_tool_to_langchain_tool(session, tool) for tool in tools.tools]
