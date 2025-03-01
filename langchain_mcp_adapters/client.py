165132e56d52e157ffbc4a29dba53cdd1a399988
vbarda <vadym@langchain.dev>
2025-03-01 09:25:45 -0500

    cr

--- langchain_mcp_adapters/client.py
+++ langchain_mcp_adapters/client.py
@@ -15,16 +15,33 @@ DEFAULT_ENCODING_ERROR_HANDLER = "strict"
 
 class StdioConnection(TypedDict):
     transport: Literal["stdio"]
+
     command: str
+    """The executable to run to start the server."""
+
     args: list[str]
+    """Command line arguments to pass to the executable."""
+
     env: dict[str, str] | None
+    """The environment to use when spawning the process."""
+
     encoding: str
+    """The text encoding used when sending/receiving messages to the server."""
+
     encoding_error_handler: Literal["strict", "ignore", "replace"]
+    """
+    The text encoding error handler.
+
+    See https://docs.python.org/3/library/codecs.html#codec-base-classes for
+    explanations of possible values
+    """
 
 
 class SSEConnection(TypedDict):
     transport: Literal["sse"]
+
     url: str
+    """The URL of the SSE endpoint to connect to."""
 
 
 class MultiServerMCPClient:
